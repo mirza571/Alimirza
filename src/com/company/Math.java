e08fa7654b361f160f5322e2ac3b9b32a0dd91b4
mirza571 <mirzaaliakbar111@gmail.com>
2019-12-03 11:13:14 +0100

    Added Sub method to the Math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -9,4 +9,8 @@ public class Math {
     public int add(int first, int second) {
         return first + second;
     }
+
+    public int sub(int first, int second) {
+        return first - second;
+    }
 }
