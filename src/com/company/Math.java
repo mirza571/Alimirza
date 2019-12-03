481de298d72db3f2d2370ae634e69e7af38927f2
mirza571 <mirzaaliakbar111@gmail.com>
2019-12-03 11:02:45 +0100

    Added Add method to the Math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Math {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int add(int first, int second) {
+        return first + second;
+    }
 }
